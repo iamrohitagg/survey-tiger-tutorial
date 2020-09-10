e93ec73e5cfa2789b63b752c9080b1a90937811b
imarohitagg <rohitaggarwal376@gmail.com>
2020-09-10 12:13:49 +0530

    initial commit

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import './_landing_page.css';
+import React from "react";
+import "./_landing_page.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const LandingPage = (props) => {
-	return (
-		<div>
-			<div>
-				<Link to="/create">
-					<button className="landing-page-button"> Create Survey</button>
-				</Link>
-			</div>
-			<div>
-				<button className="landing-page-button"> Take Survey</button>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <Link to="/create">
+          <button className="landing-page-button"> Create Survey</button>
+        </Link>
+      </div>
+      <div>
+        <button className="landing-page-button"> Take Survey</button>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
-
